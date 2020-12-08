4a20e69f9bf47f21938f7deb03667942b9872bc4
Ceshine Lee <shuanck@gmail.com>
2020-12-08 22:09:11 +0800

    Annotate base_recommender.py

--- baserec/base/base_recommender.py
+++ baserec/base/base_recommender.py
@@ -1,6 +1,8 @@
 """
 @author: Maurizio Ferrari Dacrema & Ceshine Lee
 """
+from typing import Optional, List, Union, Tuple
+
 import numpy as np
 
 from .data_io import DataIO
@@ -13,9 +15,17 @@ class BaseRecommender(object):
     RECOMMENDER_NAME = "Recommender_Base_Class"
 
     def __init__(self, URM_train, verbose=True):
+        """
+        Parameters
+        ----------
+        URM_train : [TODO:type]
+            User-Item interaction matrix
+        verbose : bool, optional
+            control verbosity, by default True
+        """
+        super().__init__()
 
-        super(BaseRecommender, self).__init__()
-
+        # Convert matrix to csr and eliminate zeros
         self.URM_train = check_matrix(URM_train.copy(), 'csr', dtype=np.float32)
         self.URM_train.eliminate_zeros()
 
@@ -28,17 +38,23 @@ class BaseRecommender(object):
         self.items_to_ignore_flag = False
         self.items_to_ignore_ID = np.array([], dtype=np.int)
 
+        # Detect empty (user) rows
         self._cold_user_mask = np.ediff1d(self.URM_train.indptr) == 0
 
         if self._cold_user_mask.any():
             self._print("URM Detected {} ({:.2f} %) cold users.".format(
-                self._cold_user_mask.sum(), self._cold_user_mask.sum()/self.n_users*100))
+                self._cold_user_mask.sum(),
+                self._cold_user_mask.sum() / self.n_users * 100))
 
+        # Detect empty (item) columns
+        # This could be an expensive operation?
         self._cold_item_mask = np.ediff1d(self.URM_train.tocsc().indptr) == 0
 
         if self._cold_item_mask.any():
             self._print("URM Detected {} ({:.2f} %) cold items.".format(
-                self._cold_item_mask.sum(), self._cold_item_mask.sum()/self.n_items*100))
+                self._cold_item_mask.sum(),
+                self._cold_item_mask.sum() / self.n_items * 100
+            ))
 
     def _get_cold_user_mask(self):
         return self._cold_user_mask
@@ -57,12 +73,15 @@ class BaseRecommender(object):
         return self.URM_train.copy()
 
     def set_URM_train(self, URM_train_new, **kwargs):
-
-        assert self.URM_train.shape == URM_train_new.shape, "{}: set_URM_train old and new URM train have different shapes".format(
-            self.RECOMMENDER_NAME)
+        # TODO: refactor the use this also in __init__
+        assert self.URM_train.shape == URM_train_new.shape, \
+            "{}: set_URM_train old and new URM train have different shapes".format(
+                self.RECOMMENDER_NAME)
 
         if len(kwargs) > 0:
-            self._print("set_URM_train keyword arguments not supported for this recommender class. Received: {}".format(kwargs))
+            self._print(
+                "set_URM_train keyword arguments not supported for this "
+                "recommender class. Received: {}".format(kwargs))
 
         self.URM_train = check_matrix(URM_train_new.copy(), 'csr', dtype=np.float32)
         self.URM_train.eliminate_zeros()
@@ -71,7 +90,7 @@ class BaseRecommender(object):
 
         if self._cold_user_mask.any():
             self._print("Detected {} ({:.2f} %) cold users.".format(
-                self._cold_user_mask.sum(), self._cold_user_mask.sum()/len(self._cold_user_mask)*100))
+                self._cold_user_mask.sum(), self._cold_user_mask.sum() / len(self._cold_user_mask) * 100))
 
     def set_items_to_ignore(self, items_to_ignore):
         self.items_to_ignore_flag = True
@@ -96,16 +115,16 @@ class BaseRecommender(object):
         return scores_batch
 
     def _remove_seen_on_scores(self, user_id, scores):
-
-        assert self.URM_train.getformat() == "csr", "Recommender_Base_Class: URM_train is not CSR, this will cause errors in filtering seen items"
-
+        """Set seen items of `user_id` in `scores` to -np.inf
+        """
+        assert self.URM_train.getformat() == "csr", \
+            "Recommender_Base_Class: URM_train is not CSR, this will cause errors in filtering seen items"
         seen = self.URM_train.indices[self.URM_train.indptr[user_id]:self.URM_train.indptr[user_id + 1]]
-
         scores[seen] = -np.inf
         return scores
 
     def _compute_item_score(self, user_id_array, items_to_compute=None):
-        """
+        """Get item scores for the (user_id, item_id) pairs
 
         :param user_id_array:       array containing the user indices whose recommendations need to be computed
         :param items_to_compute:    array containing the items whose scores are to be computed.
@@ -113,11 +132,40 @@ class BaseRecommender(object):
         :return:                    array (len(user_id_array), n_items) with the score.
         """
         raise NotImplementedError(
-            "BaseRecommender: compute_item_score not assigned for current recommender, unable to compute prediction scores")
+            "BaseRecommender: compute_item_score not assigned for current recommender, "
+            "unable to compute prediction scores"
+        )
 
-    def recommend(self, user_id_array, cutoff=None, remove_seen_flag=True, items_to_compute=None,
-                  remove_top_pop_flag=False, remove_custom_items_flag=False, return_scores=False):
+    def recommend(
+        self, user_id_array: np.array, cutoff: Optional[int] = None,
+        remove_seen_flag: bool = True, items_to_compute: Optional[np.array] = None,
+        remove_top_pop_flag: bool = False, remove_custom_items_flag: bool = False,
+        return_scores: bool = False
+    ) -> Union[Union[List[List[int]], List[int]], Union[List[List[int]], List[int]], np.array]:
+        """Make recommendations for users in user_id_array
 
+        Parameters
+        ----------
+        user_id_array : np.array
+            List of user_ids to make recommendations for
+        cutoff : Optional[int], optional
+            [description], by default None
+        remove_seen_flag : bool, optional
+            Don't recommend seen items, by default True
+        items_to_compute : np.array, optional
+            List of item_ids to include in computation, by default None
+        remove_top_pop_flag : bool, optional
+            Don't recommend top popular items, by default False
+        remove_custom_items_flag : bool, optional
+            [description], by default False
+        return_scores : bool, optional
+            Also return the score matrix, by default False
+
+        Returns
+        -------
+        Union[List, Tuple[List, np.array]]
+            ranking_list or (ranking_list, scores_batch) accorgin to return_scores
+        """
         # If is a scalar transform it in a 1-cell array
         if np.isscalar(user_id_array):
             user_id_array = np.atleast_1d(user_id_array)
@@ -126,42 +174,19 @@ class BaseRecommender(object):
             single_user = False
 
         if cutoff is None:
+            # No cutoff, take all items
             cutoff = self.URM_train.shape[1] - 1
 
         # Compute the scores using the model-specific function
         # Vectorize over all users in user_id_array
         scores_batch = self._compute_item_score(user_id_array, items_to_compute=items_to_compute)
 
-        # if self.normalize:
-        #     # normalization will keep the scores in the same range
-        #     # of value of the ratings in dataset
-        #     user_profile = self.URM_train[user_id]
-        #
-        #     rated = user_profile.copy()
-        #     rated.data = np.ones_like(rated.data)
-        #     if self.sparse_weights:
-        #         den = rated.dot(self.W_sparse).toarray().ravel()
-        #     else:
-        #         den = rated.dot(self.W).ravel()
-        #     den[np.abs(den) < 1e-6] = 1.0  # to avoid NaNs
-        #     scores /= den
-
         for user_index in range(len(user_id_array)):
-
             user_id = user_id_array[user_index]
 
             if remove_seen_flag:
+                # Don't recommend seen items
                 scores_batch[user_index, :] = self._remove_seen_on_scores(user_id, scores_batch[user_index, :])
-
-            # Sorting is done in three steps. Faster then plain np.argsort for higher number of items
-            # - Partition the data to extract the set of relevant items
-            # - Sort only the relevant items
-            # - Get the original item index
-            # relevant_items_partition = (-scores_user).argpartition(cutoff)[0:cutoff]
-            # relevant_items_partition_sorting = np.argsort(-scores_user[relevant_items_partition])
-            # ranking = relevant_items_partition[relevant_items_partition_sorting]
-            #
-            # ranking_list.append(ranking)
 
         if remove_top_pop_flag:
             scores_batch = self._remove_TopPop_on_scores(scores_batch)
@@ -169,19 +194,29 @@ class BaseRecommender(object):
         if remove_custom_items_flag:
             scores_batch = self._remove_custom_items_on_scores(scores_batch)
 
+        # Sorting is done in three steps. Faster then plain np.argsort for higher number of items
+        # - Partition the data to extract the set of relevant items
+        # - Sort only the relevant items
+        # - Get the original item index
+
         # relevant_items_partition is block_size x cutoff
-        relevant_items_partition = (-scores_batch).argpartition(cutoff, axis=1)[:, 0:cutoff]
+        # (np.partition: kth item in the results will be where it would be in a sorted array
+        #  and the smaller elements to the left and equal or greater elements to the right)
+        relevant_items_partition = (-scores_batch).argpartition(cutoff, axis=1)[:, 0: cutoff]
 
         # Get original value and sort it
         # [:, None] adds 1 dimension to the array, from (block_size,) to (block_size,1)
         # This is done to correctly get scores_batch value as [row, relevant_items_partition[row,:]]
-        relevant_items_partition_original_value = scores_batch[np.arange(
-            scores_batch.shape[0])[:, None], relevant_items_partition]
+        relevant_items_partition_original_value = scores_batch[
+            np.arange(scores_batch.shape[0])[:, None], relevant_items_partition]
         relevant_items_partition_sorting = np.argsort(-relevant_items_partition_original_value, axis=1)
-        ranking = relevant_items_partition[np.arange(relevant_items_partition.shape[0])[
-            :, None], relevant_items_partition_sorting]
+        # The ranking matrix holds Top-K item ids for each user id
+        ranking = relevant_items_partition[
+            np.arange(relevant_items_partition.shape[0])[:, None],
+            relevant_items_partition_sorting
+        ]
 
-        ranking_list = [None] * ranking.shape[0]
+        ranking_list: List[List[int]] = [[]] * ranking.shape[0]
 
         # Remove from the recommendation list any item that has a -inf score
         # Since -inf is a flag to indicate an item to remove
@@ -195,14 +230,15 @@ class BaseRecommender(object):
             ranking_list[user_index] = user_recommendation_list.tolist()
 
         # Return single list for one user, instead of list of lists
+        ranking_list_final: Union[List[List[int]], List[int]] = ranking_list
         if single_user:
-            ranking_list = ranking_list[0]
+            ranking_list_final = ranking_list[0]
 
         if return_scores:
-            return ranking_list, scores_batch
+            return ranking_list_final, scores_batch
 
         else:
-            return ranking_list
+            return ranking_list_final
 
     #########################################################################################################
     ##########                                                                                     ##########
@@ -214,7 +250,6 @@ class BaseRecommender(object):
         raise NotImplementedError("BaseRecommender: save_model not implemented")
 
     def load_model(self, folder_path, file_name=None):
-
         if file_name is None:
             file_name = self.RECOMMENDER_NAME
 
@@ -223,6 +258,7 @@ class BaseRecommender(object):
         dataIO = DataIO(folder_path=folder_path)
         data_dict = dataIO.load_data(file_name=file_name)
 
+        # Set attributes of the instance to values in `data_dict`
         for attrib_name in data_dict.keys():
             self.__setattr__(attrib_name, data_dict[attrib_name])
 
